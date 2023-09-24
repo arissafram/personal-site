f3703bc48d1369e7f74a439c33b6d6cfa26729c7
Arissa Fram <31447108+arissafram@users.noreply.github.com>
2023-09-24 17:04:53 -0400

    Change html structure (#1)
    - remove unused divs
    - make entire board droppable
    - fix styles, remove transforms

--- src/puzzle.js
+++ src/puzzle.js
@@ -1,28 +1,41 @@
 class Puzzle {
   constructor() {
-    this.totalImages = 9;
-    this.imageDropId = '';
     this.currentImageEl = null;
-    this.piecesContainer = null;
-    this.imageDropContainer = null;
-    this.zIndex = 1;
-
+    this.imageDropId = '';
+    this.totalImages = 9;
     this.offsetX = null;
     this.offsetY = null;
-
-    this.onDragstart = this.onDragstart.bind(this);
-    this.onDragover = this.onDragover.bind(this);
-    this.onDrop = this.onDrop.bind(this);
+    this.zIndex = 1;
   }
 
-  init = () => {
-    const board = document.querySelector('.board');
+   init = () => {
+    this.board = document.querySelector('.board');
+    this.board.addEventListener('drop', this.onDrop);
+    this.board.addEventListener('dragover', this.onDragover);
+    this.makeImageDropContainer();
+    this.makePuzzlePieces();
+  }
+
+  makeImageDropContainer = () => {
+    this.imageDropContainer = document.createElement('div');
+    this.imageDropContainer.classList.add('image-drop-container');
+    this.imageDropContainer.setAttribute('id', 'drop-container');
+
     let i = 0;
+    while (this.totalImages > i) {
+      i++
+      const imageDrop = document.createElement('div');
+      imageDrop.classList.add('image-drop');
+      imageDrop.setAttribute('id', `${i}-drop`);
+      imageDrop.addEventListener('dragenter', this.onDragEnter);
+      this.imageDropContainer.append(imageDrop);
+    }
 
-    this.piecesContainer = document.createElement('div');
-    this.piecesContainer.classList.add('pieces-container');
-    board.appendChild(this.piecesContainer);
+    this.board.appendChild(this.imageDropContainer);
+  }
 
+  makePuzzlePieces = () => {
+    let i = 0;
     while (this.totalImages > i) {
       i++
       const piece = document.createElement('div');
@@ -30,41 +43,23 @@ class Puzzle {
       piece.style.zIndex = 1;
       piece.setAttribute('id', i);
       piece.setAttribute('draggable', true);
+
       piece.addEventListener('dragstart', this.onDragstart);
       piece.addEventListener('drop', this.onDrop);
       piece.addEventListener('dragover', this.onDragover);
-      piece.style.top = `${this.getRandomPositionString(false)}px`;
-      piece.style.left = `${this.getRandomPositionString(true)}px`;
+
+      piece.style.top = `${this.getRandomPositionString(true)}px`;
+      piece.style.left = `${this.getRandomPositionString(false)}px`;
       piece.style.backgroundImage = `url('./public/puzzle_${i}.jpg')`;
-      this.piecesContainer.append(piece);
-    }
 
-    this.imageDropContainer = document.createElement('div');
-    this.imageDropContainer.classList.add('image-drop-container');
-    this.imageDropContainer.setAttribute('id', 'drop-container');
-    this.imageDropContainer.addEventListener('drop', this.onDrop);
-    this.imageDropContainer.addEventListener('dragover', this.onDragover);
-    board.appendChild(this.imageDropContainer);
-    let n = 0;
-    while (this.totalImages > n) {
-      n++
-      const imageDrop = document.createElement('div');
-      imageDrop.classList.add('image-drop');
-      imageDrop.setAttribute('id', `${n}-drop`);
-      imageDrop.addEventListener('dragenter', this.onDragEnter);
-      this.imageDropContainer.append(imageDrop);
+      this.board.append(piece);
     }
-
-    // setTimeout(() => this.imageDropContainer.classList.add('complete'), 500)
   }
-
 
   onDragstart = (e) => {
     this.currentImageEl = e.target;
-
-    const board = document.querySelector('.board');
-    this.offsetX = (e.clientX + board.getBoundingClientRect().left) - this.currentImageEl.getBoundingClientRect().left;
-    this.offsetY = (e.clientY + board.getBoundingClientRect().top) - this.currentImageEl.getBoundingClientRect().top;
+    this.offsetX = e.clientX - e.target.getBoundingClientRect().left;
+    this.offsetY = e.clientY - e.target.getBoundingClientRect().top;
   }
 
   onDragover = (e) => {
@@ -74,13 +69,11 @@ class Puzzle {
 
   onDrop = (e) => {
     e.preventDefault();
+    const x = e.clientX - this.offsetX - this.board.getBoundingClientRect().left;
+    const y = e.clientY - this.offsetY - this.board.getBoundingClientRect().top;
 
-    const newX = e.clientX - this.offsetX;
-    const newY = e.clientY - this.offsetY;
-
-    this.currentImageEl.style.left = `${newX}px`;
-    this.currentImageEl.style.top = `${newY}px`;
-    this.currentImageEl.style.position = 'absolute';
+    this.currentImageEl.style.left = `${x}px`;
+    this.currentImageEl.style.top = `${y}px`;
 
     this.zIndex += 1;
     this.currentImageEl.style.zIndex = this.zIndex;
@@ -101,14 +94,10 @@ class Puzzle {
     this.currentImageEl.style.top = '0px';
   }
 
-  getRandomPositionString = (top) => {
-    const num = Math.floor(Math.random() * (top ? 300 : 330));
+  getRandomPositionString = (isHeight) => {
+    const boardHeight = parseFloat(getComputedStyle(this.board).height);
+    const boardWidth = parseFloat(getComputedStyle(this.board).width);
+    const num = Math.floor(Math.random() * (isHeight ? boardHeight : boardWidth));
     return num.toString();
-  }
-
-  checkWin = () => {
-    if (!this.piecesContainer.children.length) {
-      this.imageDropContainer.classList.add('complete');
-    }
   }
 }
