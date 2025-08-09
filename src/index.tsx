df792e2e2f2e1e58d0bb4d857ab007ed131d7149
Arissa Fram <arissafram@gmail.com>
2025-08-09 18:02:46 -0400

    update puzzle props

--- src/index.tsx
+++ src/index.tsx
@@ -21,8 +21,32 @@ function App() {
     <main className="main">
       <div className="game-container">
         <Puzzle
-          image="/photo.jpg"
-          options={PUZZLE_OPTIONS}
+          image="/puzzle.jpg"
+          options={{
+            board: {
+              columns: 4,
+              rows: 3,
+              width: 400,
+              height: 450,
+              outlineStrokeColor: "red",
+              snapThreshold: 20,
+              scatterArea: 50,
+              showBoardSlotOutlines: true,
+            },
+            puzzle: {
+              responsive: true,
+              timer: {
+                enabled: true,
+              },
+              refreshButton: {
+                enabled: true,
+              },
+            },
+            puzzlePiece: {
+              strokeColor: "#32cd32",
+              strokeEnabled: true,
+            },
+          }}
           onComplete={handleComplete}
           onRefresh={handleRefresh}
           responsive={true}
