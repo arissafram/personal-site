54ff9bf1b3575c6147483209547fbac904d097fa
Arissa Fram <arissafram@gmail.com>
2025-08-09 20:35:01 -0400

    clean up

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,11 @@
 import "./styles.css";
 
 interface HeaderProps {
-  className?: string;
+  className: "desktop-header" | "mobile-header";
 }
 
 const Header = (props: HeaderProps) => {
-  const { className = "" } = props;
+  const { className } = props;
 
   return (
     <header>
--- src/index.tsx
+++ src/index.tsx
@@ -10,20 +10,20 @@ import "./index.css";
 const App = () => {
   return (
     <main className="main">
-      <Header className="mobileHeader" />
+      <Header className="mobile-header" />
       <div className="puzzle-container">
         <Puzzle
           image="/puzzle.jpg"
           options={{
             board: {
               columns: 3,
-              rows: 3,
-              width: 400,
               height: 450,
               outlineStrokeColor: "#a9a9a9",
+              rows: 3,
               snapThreshold: 20,
               scatterArea: 50,
               showBoardSlotOutlines: true,
+              width: 400,
             },
             puzzle: {
               responsive: true,
@@ -43,7 +43,7 @@ const App = () => {
         />
       </div>
       <div className="content-container">
-        <Header className="desktopHeader" />
+        <Header className="desktop-header" />
         <Blurb />
         <Links />
       </div>
