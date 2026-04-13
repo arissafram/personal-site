4d03bc31a5e2b769cb95dbd700c7c6bd2d1ec9cf
Arissa Fram <arissafram@gmail.com>
2026-04-13 10:25:40 -0400

    update content and styles

--- src/components/blurb/index.tsx
+++ src/components/blurb/index.tsx
@@ -3,17 +3,19 @@ import "./styles.css";
 const Blurb = () => {
   return (
     <section className="blurb-container">
-      <p className="paragraph">Senior software engineer @ Underdog Fantasy</p>
-      <p className="paragraph promocode">Promocode: BIGRISS</p>
+      <p className="paragraph">
+        Product engineer @{" "}
+        <a className="link" href="https://play.underdogfantasy.com/bigriss-bbbdfc02f9d75f4b">Underdog</a>
+      </p>
+      <p className="paragraph">
+        Fullstack engineer with a expertise in web technologies: React, TypeScript, NodeJS, REST/GraphQL,
+        Storybook, testing libraries, etc, etc.
+      </p>
       <p className="paragraph">
         I like puzzles and live music. I study French. I live in Brooklyn. I
         value meaningful projects, thorough code review and winning as a team.
-        Currently supporting Underdog web products, building fun things on my
-        own time and adding to a collection of useless domains.
-      </p>
-      <p className="paragraph">
-        NodeJS, React, Typescript, Javascript, Webpack, NextJS, REST/Graphql,
-        testing libraries, etc. etc., iOS dreams.
+        Currently supporting Underdog products, building fun things on my
+        own time, and adding to a collection of useless domains.
       </p>
     </section>
   );
