e6602a7ed97374ba43740a65c0d4c1fbfc2f681d
Arissa Fram <arissafram@gmail.com>
2026-04-17 18:24:37 -0400

    update blurb

--- src/components/blurb/index.tsx
+++ src/components/blurb/index.tsx
@@ -8,14 +8,14 @@ const Blurb = () => {
         <a className="link" href="https://play.underdogfantasy.com/bigriss-bbbdfc02f9d75f4b">Underdog</a>
       </p>
       <p className="paragraph">
-        Fullstack engineer with expertise in web technologies: React, TypeScript, NodeJS, REST/GraphQL,
-        Storybook, testing libraries, etc, etc.
-      </p>
-      <p className="paragraph">
         I like puzzles and live music. I study French. I live in Brooklyn. I
         value meaningful projects, thorough code review and winning as a team.
         Currently supporting Underdog products, building fun things on my
         own time, and adding to a collection of useless domains.
+      </p>
+      <p className="paragraph">
+        Fullstack product engineer, with an expertise in Web. Typescript, React/RN, NodeJS, 
+        Swift, Kotlin, Rails, REST/GraphQL, Storybook, testing libraries.
       </p>
     </section>
   );
